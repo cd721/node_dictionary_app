baaac97ad645b60665dfb70880b11707610b556a
cd721 <cdemario@stevens.edu>
2024-11-13 18:40:05 -0500

    change port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log('Your routes will be running on http://localhost:5000');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,43 @@
+import axios from 'axios'
 const constructorMethod = (app) => {
-    app.use('/', userRoutes);
-    app.get(async (req, res) => {
+    app.get("/",async (req, res) => {
         try {
 
-
-
-            return res.status(200).json({});
+            return res.status(200).render("index",{});
         } catch (e) {
             return res.status(500).json({ error: e })
         }
     })
-        .get('/:word', async (req, res) => {
-            try {
-                if (req.params.word.trim() == "") {
-                    throw "Sorry, you didn't enter valid text"
+    app.get('/:word', async (req, res) => {
+        try {
+            if (req.params.word.trim() == "") {
+                throw "Sorry, you didn't enter valid text"
+            }
+
+
+        } catch (e) {
+            return res.status(400).json({ error: e });
+        }
+
+
+        try {
+            console.log(req.params.word)
+            let base_url = 
+            `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${req.params.word}`;
+            
+            
+            let { data } =await  axios.get(base_url, {
+                params: {
+                    'key': `${process.env.API_KEY}`
                 }
+            })
 
-
-            } catch (e) {
-                return res.status(400).json({ error: e });
-            }
-
-
-            try {
-                let base_url = $`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${req.params.word}`;
-                let { data } = axios.get(base_url, {
-                    headers: {
-                        Authorization: $`Bearer ${process.env.API_KEY}`
-                    }
-                })
-                console.log(data)
-                return res.status(200).json({});
-            } catch (e) {
-                res.status(404).json({ error: e });
-            }
-        });
+            console.log(data)
+            return res.status(200).json({});
+        } catch (e) {
+            res.status(500).json({ error: e });
+        }
+    });
     app.use('*', (req, res) => {
         res.redirect('/');
     });
