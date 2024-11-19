e9798185b785ae55cbe72d61619ebf8ef856e421
cd721 <cdemario@stevens.edu>
2024-11-19 11:59:41 -0500

    update word route

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,49 @@
 import axios from 'axios'
 const constructorMethod = (app) => {
-    app.get("/",async (req, res) => {
+    app.get("/", async (req, res) => {
         try {
 
-            return res.status(200).render("index",{});
+            return res.status(200).render("index", {});
         } catch (e) {
             return res.status(500).json({ error: e })
         }
     })
     app.get('/:word', async (req, res) => {
-        try {
-            if (req.params.word.trim() == "") {
-                throw "Sorry, you didn't enter valid text"
-            }
-
-
-        } catch (e) {
+        let word = req.params.word;
+        word = word.trim();
+        if (word === "") {
             return res.status(400).json({ error: e });
         }
 
 
         try {
-            console.log(req.params.word)
-            let base_url = 
-            `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${req.params.word}`;
-            
-            
-            let { data } =await  axios.get(base_url, {
+            console.log(word)
+            let base_url =
+                `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}`;
+
+
+            let { data } = await axios.get(base_url, {
                 params: {
                     'key': `${process.env.API_KEY}`
                 }
             })
 
+            if (data == []) {
+                console.log("User entered an invalid word. The Dictionary API did not provide suggestions.")
+                return res.status(404).render("invalid_word", {})
+            } else if (Array.isArray(data) && typeof data[0] === 'string') {
+                const suggestions = data;
+                console.log("User entered an invalid word. The Dictionary API did not provide suggestions.")
+                return res.status(302).render("invalid_word", { suggestions: suggestions });
+
+            }
             console.log(data)
-            return res.status(200).json({});
+            shortdef = data[0]['shortdef']
+
+            return res.status(200).render("word.html", { word: word, definitions: shortdef });
+
+
+
         } catch (e) {
             res.status(500).json({ error: e });
         }
